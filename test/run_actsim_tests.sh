71af8490a9dc3c98736065794a4c3c472d48cd67
Fabian Posch <fabian@posch.tech>
2024-02-08 15:59:07 -0500

    change test script to output failed test information and use new output

--- test/run_actsim_tests.sh
+++ test/run_actsim_tests.sh
@@ -11,18 +11,18 @@ myecho()
 {
   if [ $check_echo -eq 0 ]
   then
-	check_echo=1
-	count=`echo -n "" | wc -c | awk '{print $1}'`
-	if [ $count -gt 0 ]
-	then
-		check_echo=2
-	fi
+    check_echo=1
+    count=`echo -n "" | wc -c | awk '{print $1}'`
+    if [ $count -gt 0 ]
+    then
+        check_echo=2
+    fi
   fi
   if [ $check_echo -eq 1 ]
   then
-	echo -n "$@"
+    echo -n "$@"
   else
-	echo "$@\c"
+    echo "$@\c"
   fi
 }
 
@@ -39,68 +39,75 @@ fn_actsim_script="test.actsim"
 
 for subdir in ./*/
 do
-	if [ -d $subdir ]; then
-		cd "$subdir"
+    if [ -d $subdir ]; then
+        cd "$subdir"
 
-		if [ $num -lt 10 ]
-		then
-			myecho ".[0$num]"
-		else
-			myecho ".[$num]"
-		fi
+        if [ $num -lt 10 ]
+        then
+            myecho ".[0$num]"
+        else
+            myecho ".[$num]"
+        fi
 
-		# simulate
-		actsim $fn_actfile $process_name < $fn_actsim_script > $process_name.stdout 2> $process_name.stderr
+        # simulate
+        actsim $fn_actfile $process_name < $fn_actsim_script > $process_name.stdout 2> $process_name.stderr
 
-		# strip timing info from log
-        sed 's/\[.*\]//g' $process_name.stdout > $process_name.processed
+        count=`expr $count + 1`
+        num=`expr $num + 1`
 
-		count=`expr $count + 1`
-		num=`expr $num + 1`
+        ok=1
 
-		# test whether output matches ground truth and report
-		ok=1
-		if ! cmp $process_name.processed $process_name.truth >/dev/null 2>/dev/null
-		then
-			echo 
-			myecho "** FAILED TEST $subdir$fn_actfile: stdout mismatch"
-			fail=`expr $fail + 1`
-			ok=0
-		fi
+        # set the seperator tokens to only newline, so we an iterate over grep output
+        oldifs=$IFS
+        IFS=$'\n'
 
-		if [ $ok -eq 1 ]
-		then
-			if [ $num -eq $lim ]
-			then
-				echo 
-				myecho " "
-				num=0
-			fi
-		else
-			echo " **"
-			myecho " "
-			num=0
-		fi
-		cd ..
-	fi
+        # check if there are any test failures reported in the sim log and report
+        for line in $(grep "TEST FAILED" "$process_name.stdout")
+        do
+            echo
+            myecho "** FAILED TEST $subdir$fn_actfile: $(echo $line | sed 's/\[.*TEST FAILED ([[:digit:]]*): //g') **"
+            num=0
+            fail=`expr $fail + 1`
+            ok=0
+        done
+
+        # reset the seperator tokens
+        IFS=$oldifs
+
+        if [ $ok -eq 1 ]
+        then
+            if [ $num -eq $lim ]
+            then
+                echo 
+                myecho " "
+                num=0
+            fi
+        else
+            echo
+            echo
+            myecho " "
+            num=0
+        fi
+        cd ..
+    fi
 done
 
 if [ $num -ne 0 ]
 then
-	echo
+    echo
 fi
 
 if [ $fail -ne 0 ]
 then
-	if [ $fail -eq 1 ]
-	then
-		echo "--- Summary: 1 test failed ---"
-	else
-		echo "--- Summary: $fail tests failed ---"
-	fi
-	exit 1
+    if [ $fail -eq 1 ]
+    then
+        echo "--- Summary: 1 test failed ---"
+    else
+        echo "--- Summary: $fail tests failed ---"
+    fi
+    exit 1
 else
-	echo
-	echo "SUCCESS! All tests passed."
+    echo
+    echo "SUCCESS! All tests passed."
 fi
 echo
