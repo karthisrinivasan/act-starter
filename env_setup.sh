7b4ad6c9b20867c200f449fe9bca8ce557eb8492
Fabian Posch <fabian@posch.tech>
2024-02-08 17:50:28 -0500

    make env setup script location independent and only export if src folder not already in path

--- env_setup.sh
+++ env_setup.sh
@@ -1,3 +1,24 @@
 #! /bin/sh
 
-export ACT_PATH=$(pwd)/src:$ACT_PATH
+# This needs to be called with source!
+# Executing this script will not work!
+# You cannot export from within a sub-shell
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+SRC_DIR=$SCRIPT_DIR/src
+
+found=0
+
+IFS=':'
+for elem in $ACT_PATH
+do
+    if [[ "$elem" == "$SRC_DIR" ]]; then
+        found=1
+    fi
+done
+
+if [ $found -ne 1 ]
+then
+    export ACT_PATH=$SRC_DIR:$ACT_PATH
+fi
